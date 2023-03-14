11afe70fdf55fb4dd09957429d48e1e6a0ab3987
Azizbek <Uygurskiy93@gmail.com>
2023-03-14 16:46:35 +0600

    12112

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,78 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
-import Card from './components/Card'
+import Card from './components/Card';
 import Drawer from './components/Drawer';
 import Header from './components/Header';
+
+
+
+const arr = [
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    image: "/img/k1.jpg"
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    image: "/img/k2.jpg"
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    image: "/img/k3.jpg"
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    image: "/img/k4.jpg"
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    image: "/img/k5.jpg"
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    image: "/img/k6.jpg"
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    image: "/img/k7.jpg"
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    image: "/img/k8.jpg"
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    image: "/img/k9.jpg"
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    image: "/img/k10.png"
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    image: "/img/k11.png"
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    image: "/img/k12.jpg"
+  }
+]
 
 
 function App() {
   return (
     <div className="wrapper clear">
-      <Drawer />
       <Header />
       <div className='content p-40'>
         <div className='d-flex align-center mb-40 justify-between'>
@@ -17,11 +82,12 @@ function App() {
             <input type="text" placeholder='search...' />
           </div>
         </div>
-        <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="d-flex flex-wrap">
+          {
+            arr.map((sneak) => (
+              <Card title={sneak.title} price={sneak.price} image={sneak.image} />
+            ))
+          }
         </div>
       </div>
     </div>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
-function Card() {
+
+function Card(props) {
     return (
         <div className='cart'>
             <div className='favorite'>
                 <img src="/img/heart1.svg" alt="Heart" />
             </div>
-            <img width={133} height={112} src="/img/k1.jpg" alt="" />
-            <h5 className='mb-10 mt-10'>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <img width={133} height={112} src={props.image} alt="" />
+            <h5 className='mb-10 mt-10'>{props.title}</h5>
             <div className='d-flex justify-between align-center'>
                 <div className='d-flex flex-column'>
                     <span>Цена</span>
-                    <b>12 999 rub.</b>
+                    <b>{props.price} rub.</b>
                 </div>
                 <button className='button'>
                     <img width={11} height={11} src="/img/plus.svg" alt="plus" />
